80c4df27d2046cfe3698ae9a714af01ee365fb26
khoapham@safeforweb.com <khoapham@safeforweb.com>
2023-10-24 22:34:12 +0700

    Thêm giao diện check in/out trong giờ làm việc, mà hình đăng nhập

---
+++ CheckinManagementSystem/UCInOutCA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CheckinManagementSystem
+{
+	public partial class UCInOutCA : UserControl
+	{
+		private static UCInOutCA _instance;
+		public static UCInOutCA Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new UCInOutCA();
+				return _instance;
+			}
+		}
+		public UCInOutCA()
+		{
+			InitializeComponent();
+		}
+
+		private void UCInOutCA_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
